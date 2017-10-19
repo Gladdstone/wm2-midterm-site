9b66d8b73ddd897a3619b2d768f2e94e93a83937
Joseph Daniel Farrell <jdf4921@banjo.rit.edu>
2017-10-19 14:31:03 -0400

    New nav bar on desktop, placeholder text

--- index.php
+++ index.php
@@ -15,6 +15,9 @@
 <body>
 	<div id="content">
 		<?php include "navbar.html" ?>
+		<h1>City of Los Angeles</h1>
+		<br/>
+		<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 	</div>
 </body>
 </html>
