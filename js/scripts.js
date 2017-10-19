f3c603974d2c1d9cbefabbd398cd85aa1143f2c3
Joe Farrell <jdf4921@rit.edu>
2017-10-19 17:42:38 -0400

    Mobile navbar implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-function menuAnimate(elem) {
+function openMenu(elem) {
 	elem.classList.toggle("change");
+	openNav()
+}
+
+function openNav() {
+    document.getElementById("mobilemenu").style.width = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mobile_menu_button").classList.toggle("change");
+    document.getElementById("mobilemenu").style.width = "0";
 }
