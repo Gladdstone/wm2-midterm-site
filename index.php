0949acdaff0ef02bbdb65d09ccfa9fac3f7d1210
Joseph Daniel Farrell <jdf4921@banjo.rit.edu>
2017-10-20 20:49:49 -0400

    Layout adjustments and copyright

--- index.php
+++ index.php
@@ -18,14 +18,16 @@
 		<?php include "navbar.html" ?>
 		<h1>Southern California</h1>
 		<br/>
-		<div class="fltlft iblock">
+		<div class="fltlft">
 			<p>Known best for its beautiful coastline, and spectacular Mexican food, the southernmost region of California is composed of 8 counties - Imperial, Orange, Riverside, Ventura, Saint Barbara, San Diego, San Bernardino, and Los Angeles.</p>
-			<p>In addition to being home to much of the American cinema industry, southern California also contains two of the most populous cities in the United States. Los Angeles ranks at number two, behind New York City with a population of just under four million, and San Diego at number eight at approximately 1.4 million people.
-		</div>
-		<div class="fltrt iblock">
-			<img src="images/mountain.png">
+			<p><img src="images/mountain.png" class="fltrt">In addition to being home to much of the American cinema industry, southern California also contains two of the most populous cities in the United States. Los Angeles ranks at number two, behind New York City with a population of just under four million, and San Diego at number eight at approximately 1.4 million people.
 		</div>
 		<p></p>
+		<br/>
 	</div>
+	<?php 
+		include "footer.html"; 
+		echo "<script src='js/scripts.js'>setYear();</script>";
+	?>
 </body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,19 @@ function openMenu(elem) {
 }
 
 function openNav() {
-    document.getElementById("mobilemenu").style.width = "250px";
+	document.getElementById("mobilemenu").style.width = "250px";
 }
 
 function closeNav() {
 	document.getElementById("mobile_menu_button").classList.toggle("change");
-    document.getElementById("mobilemenu").style.width = "0";
+	document.getElementById("mobilemenu").style.width = "0";
+}
+
+function setYear() {
+	alert("test");
+	var today = new Date();
+	var year = today.getFullYear();
+
+	var text = $("copyright").innerHTML;
+	$("copyright").innerText = text + " " + year;
 }
