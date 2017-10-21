2ea1016d5ace98feaa75659c87c84bafeea83aac
Joseph Daniel Farrell <jdf4921@banjo.rit.edu>
2017-10-21 19:16:46 -0400

    Fixed display width bug

--- index.php
+++ index.php
@@ -20,11 +20,11 @@
 		<br/>
 		<div class="fltlft">
 			<p>Known best for its beautiful coastline, and spectacular Mexican food, the southernmost region of California is composed of 8 counties - Imperial, Orange, Riverside, Ventura, Saint Barbara, San Diego, San Bernardino, and Los Angeles.</p>
-			<p><img src="images/mountain.png" class="fltrt">In addition to being home to much of the American cinema industry, southern California also contains two of the most populous cities in the United States. Los Angeles ranks at number two, behind New York City with a population of just under four million, and San Diego at number eight at approximately 1.4 million people.
+			<p><img src="images/mountain.png" class="auto_img fltrt">In addition to being home to much of the American cinema industry, southern California also contains two of the most populous cities in the United States. Los Angeles ranks at number two, behind New York City with a population of just under four million, and San Diego at number eight at approximately 1.4 million people.</p>
 		</div>
 		<p></p>
 		<br/>
 	</div>
-	<?php include "footer.html"; ?>
+	<?php include "footer.html" ?>
 </body>
 </html>
