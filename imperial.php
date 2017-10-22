9ace6021cdcd3cc453b23816c6b2d5ff4386408b
Joseph Daniel Farrell <jdf4921@banjo.rit.edu>
2017-10-22 17:48:06 -0400

    Imperial

---
+++ imperial.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="Joseph Farrell">
+	<meta name="keywords" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<script src="js/scripts.js"></script>
+</head>
+<body>
+	<div id="content">
+		<?php include "navbar.html" ?>
+		<h1>Imperial</h1>
+		<h4>Largest City: <i>El Centro</i></h4>
+		<hr>
+		<br/><br/>
+		<div class="col2 fltlft pdrt">
+			<h4>Reasons to Visit</h4>
+			<hr/>
+			<p>Coming soon</p>
+		</div>
+		<div class="col2 fltrt pdlft">
+			<figure>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Blueangelsformationpd.jpg/320px-Blueangelsformationpd.jpg">
+				<figcaption>Blue Angels, source: <i>Wikipedia</i></figcaption>
+			</figure>
+		</div>
+		<?php include "footer.html" ?>
+	</div>
+</body>
+</html>
